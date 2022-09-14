339cf6215f9de85b80e25ecc04d8e4e0a5c100c1
Dakota Parker <dakotacolorado2@gmail.com>
2022-09-13 22:38:05 -0700

    Add game specs

---
+++ src/test/scala/ChineseCheckersGame/ChineseCheckersGameSpec.scala
@@ -0,0 +1,12 @@
+package ChineseCheckersGame
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.wordspec.AnyWordSpec
+
+class ChineseCheckersGameSpec extends AnyWordSpec with GivenWhenThen  {
+  "a chinese checkers game" should {
+    "start with "
+
+  }
+
+}
--- src/test/scala/ChineseCheckersGame/HexagramSpec.scala
+++ src/test/scala/ChineseCheckersGame/HexagramSpec.scala
@@ -3,31 +3,38 @@ package ChineseCheckersGame
 import org.scalatest.GivenWhenThen
 import org.scalatest.wordspec.AnyWordSpec
 
-class HexagramSpec extends AnyWordSpec with GivenWhenThen {
-  "a hexagram of radius 4" must {
+class HexagramSpec extends AnyWordSpec with GivenWhenThen  {
+  def hexagrams = (1 to 10).map(r => new Hexagram(r))
+
+  "a hexagram" must {
+    "be centered on the origin" in {
+      val hexagramCentroids = this.hexagrams.map(
+        H => H.hexagramPoints.map(p => p._1 + p._2).sum
+      )
+      hexagramCentroids.foreach(c => assert(c == 0))
+    }
+    "be symmetric about the origin" in {
+      this.hexagrams.foreach(
+        H => ( H.hexagramPoints.foreach(
+            p => ( assert(H.hexagramPoints contains (-p._1, -p._2)) )
+        ))
+      )
+    }
+    "not contain duplicate points" in {
+      this.hexagrams.foreach(H =>
+        H.hexagramPoints.groupBy(p => p).foreach(
+          g => assert(g._2.length == 1)
+        ))
+    }
+  }
+  "a hexagram of radius = r" must {
     "not contain points" which {
-      "absolute sum to greater than " in {
-        Given("A hexagram of radius 4")
-        val hexagram = new Hexagram(4)
-        assert(1==1)
-
-      }
-      "absolute" in {
-        assert(1==1)
-
+      "absolute sum to greater than 3*r" in {
+        this.hexagrams.foreach( H=>
+          H.hexagramPoints.foreach(
+            p => assert(p._1.abs + p._2.abs <= H.radius * 3)
+          ))
       }
     }
   }
 }
-
-/**
- * The starting positions are the six triangular corners of the hexagram.
- */
-//private val startingPositions: List[List[(Int, Int)]] = List (
-//  List ((0, 0), (0, 1), (1, 0), (1, 1), (0, 2), (2, 0), (1, 2), (2, 1), (0, 3), (3, 0) ),
-//  List ((4, - 4), (4, - 3), (5, - 3), (4, - 2), (5, - 2), (6, - 2), (4, - 1), (5, - 1), (6, - 1), (7, - 1) ),
-//  List ((12, 0), (11, 0), (11, 1), (10, 0), (10, 1), (10, 2), (9, 0), (9, 1), (9, 2), (9, 3) ),
-//  List ((8, 8), (8, 7), (7, 8), (7, 7), (8, 6), (6, 8), (7, 6), (6, 7), (8, 5), (5, 8) ),
-//  List ((0, 12), (0, 11), (1, 11), (0, 10), (1, 10), (2, 10), (0, 9), (1, 9), (2, 9), (3, 9) ),
-//  List ((- 4, 4), (- 3, 4), (- 3, 5), (- 2, 4), (- 2, 5), (- 2, 6), (- 1, 4), (- 1, 5), (- 1, 6), (- 1, 7) ),
-//  ).map (_.map (p => (p._1 - 4, p._2 - 4) ) )
